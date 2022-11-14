5805876079ee53c6088fbdc894561df08be60a96
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-11-15 03:27:18 +0600

    pushed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://newfinal:oRSQXgn3Hm8ceg1u@cluster0.uxgzc97.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uxgzc97.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
